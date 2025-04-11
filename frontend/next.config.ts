393aef1587a3c82606e4cf20bccf7ad1c7b35964
N7GG4 <gaurshaurya1234567890@gmail.com>
2025-04-11 18:07:44 +0530

    changes committed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,10 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   async rewrites() {
+    const backendUrl = process.env.BACKEND_URL || 'http://localhost:3000';
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*` || 'http://localhost:5000/api/:path*'
+        destination: `${backendUrl}/api/:path*`
       }
     ]
   },
